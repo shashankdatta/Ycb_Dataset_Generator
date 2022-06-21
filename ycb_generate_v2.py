bea143f17aabffe5951ef37fdb6ecf2fd4c68ad7
shashankdatta <shashankdatta@vt.edu>
2022-06-20 23:48:37 -0400

    New changes to script

--- ycb_generate_v2.py
+++ ycb_generate_v2.py
@@ -1,59 +1,71 @@
-import glob, os, time, torch
+import glob, os, time, torch, uuid
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2 as cv
 
 def main():
     ycb_download_location = f'{os.getcwd()}/models/ycb'
+    object_class = 0
     for object_name in os.listdir(ycb_download_location):
-        object_class = 0
         i = 0
-        for mask_name, img_name in zip(sorted(os.listdir(f'{ycb_download_location}/{object_name}/masks'), key=parse), 
-            glob.glob(f'{ycb_download_location}/{object_name}/*.jpg')):
+        masks_directory_location = f'{ycb_download_location}/{object_name}/masks' 
+        for mask_name, img_name in zip(sorted(os.listdir(masks_directory_location), key=maskParseFilter), 
+            sorted(glob.glob1(f'{ycb_download_location}/{object_name}', '*.jpg'), key=imgParseFilter)):
+            
+            # print(mask_name, img_name)
 
-            print(mask_name, img_name)
-
-            # img = cv.imread(f'{os.getcwd()}/masks/{imgName}', -1)
+            img = cv.imread(f'{masks_directory_location}/{mask_name}', -1)
             # print(imgName)
-            # imgs = cv.cvtColor(img.copy(), cv.COLOR_BGR2RGB)
-            # imgRGB = cv.cvtColor(imgs, cv.COLOR_RGB2GRAY)
-            # thresh = cv.threshold(imgRGB, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+            img_rgb = cv.cvtColor(img.copy(), cv.COLOR_BGR2RGB)
+            img_gray = cv.cvtColor(img_rgb, cv.COLOR_RGB2GRAY)
+            thresh = cv.threshold(img_gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
 
-            # ROI_number = 0
-            # contours = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            ROI_number = 0
+            contours = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
             # print(len(contours))
-            # contours = contours[0] if len(contours) == 2 else contours[1]
+            contours = contours[0] if len(contours) == 2 else contours[1]
 
-            # x,y,w,h = cv.boundingRect(contours[len(contours) - 1])
+            x,y,w,h = cv.boundingRect(contours[len(contours) - 1])
 
             ## Drawing Rectangle Bounding Box On Images
-            # cv.rectangle(imgs, (x - 30, y - 30), (x + w + 65, y + h + 65), (255,0,0), 5)
+            # cv.rectangle(img_rgb, (x, y), (x + w, y + h), (255,0,0), 5)
+            # cv.rectangle(img_rgb, (x - 30, y - 30), (x + w + 65, y + h + 65), (255,0,0), 5)
             # print([x - 30, y - 30, x + w + 30, y + h + 30])
             
             # print(img.shape)
-            # w_img = img.shape[1]
-            # h_img = img.shape[0]
+            w_img = img.shape[1]
+            h_img = img.shape[0]
             
+            normalizedBBoxCoordinates = normalize_bbox(object_class, x, y, w, h, w_img, h_img) 
             # normalizedBBoxCoordinates = normalize_bbox(0, x - 35, y - 35, w + 80, h + 90, w_img, h_img)
-            # normalizedBBoxCoordinates = normalize_bbox(object_class, x, y, w, h, w_img, h_img) 
 
             # print (" ".join(map(str, normalizedBBoxCoordinates)))
-            # print(normalize_bbox_data_to_im(bbox, rows, cols))
+            file_unique_id = uuid.uuid1()
+            folder_path = os.path.join(ycb_download_location,object_name,"labels")
+            file_path = os.path.join(folder_path, object_name + '.' + str(file_unique_id) + '.txt')
             
-            ## for c in contours:
+            if not os.path.exists(folder_path):
+                os.mkdir(folder_path)
+
+            with open(file_path, "w") as file:
+                file.write(" ".join(map(str, normalizedBBoxCoordinates)))
+
+
+            ## For Finding All Contours:
+            # for c in contours:
             #     x,y,w,h = cv.boundingRect(c)
             #     print([x,y,w,h])
-            #     cv.rectangle(imgs, (x - 25, y - 25), (x + w + 25, y + h + 25), (255,0,0), 5)
+            #     cv.rectangle(img_rgb, (x - 25, y - 25), (x + w + 25, y + h + 25), (255,0,0), 5)
 
             ## For Matplotlib Plots:
             # fig = plt.figure(figsize = (7, 7))
             # ax = fig.add_subplot(111)
-            # ax.imshow(imgs)
+            # ax.imshow(img_rgb)
             # plt.show(block=False)
             
             ## For cv Image View:
-            # i += 1
-            # img = cv.resize(imgs, (960, 540))
+            i += 1
+            # img = cv.resize(img_rgb, (960, 540))
             # cv.imshow(f"Image {i}", img)
             
             # if (i == 3):
@@ -67,11 +79,15 @@ def main():
             ## For cv Image View:
             # cv.waitKey(1000) 
             # cv.destroyAllWindows()
-            object_class += 1
+        object_class += 1
 
-def parse(fname):
+def maskParseFilter(fname):
     prefix, n1, n2 = fname.split('_')
     return (prefix, int(n1))
+
+def imgParseFilter(fname):
+    prefix, n1 = fname.split('_')
+    return (prefix, int(n1.split('.')[0]))
 
 def normalize_bbox(label_index, xmin, ymin,
     w, h, w_img, h_img):
